cc9b79f3c6310909150a1072383d605dbc430b9a
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-10-20 02:47:16 +0200

    ✨ Add dataframe extraction module

---
+++ pfe_preprocessing/dataframe_extraction/day.py
@@ -0,0 +1,43 @@
+import datetime
+
+import pandas as pd
+from pfe_preprocessing.data_completion.generation import complete_data
+
+
+def extract_a_day(df: pd.DataFrame, day: datetime.date) -> pd.DataFrame:
+    """
+    Use index which is a timestamp to extract a day from a DataFrame
+
+    Args:
+        df (pd.DataFrame): The DataFrame to extract a day from.
+        day (datetime.date): The day to extract.
+
+    Returns:
+        pd.DataFrame: The extracted day.
+    """
+    return df[df.index.date == day]  # type: ignore
+
+
+def split_by_day(data: pd.DataFrame) -> list[pd.DataFrame]:
+    """
+    Split data by day
+    """
+    grouped = data.groupby(data.index.date)  # type: ignore
+    data_by_day = [group for _, group in grouped]
+    return data_by_day
+
+
+def complete_a_day(
+    df: pd.DataFrame, day: datetime.date, interval: int = 15
+) -> pd.DataFrame:
+    """
+    Complete a day in a DataFrame by adding new data points between existing data points.
+    """
+    day_df = extract_a_day(df, day)
+
+    if len(day_df) == 0:
+        raise ValueError("Day is not in DataFrame")
+
+    completed_df = complete_data(day_df, interval_seconds=interval)
+
+    return completed_df
---
+++ pfe_preprocessing/dataframe_extraction/year.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from pfe_preprocessing.data_completion.generation import complete_data
+from pfe_preprocessing.dataframe_extraction import day
+
+
+def extract_a_year(df: pd.DataFrame, year: int) -> pd.DataFrame:
+    """
+    Use index which is a timestamp to extract a year from a DataFrame
+
+    Args:
+        df (pd.DataFrame): The DataFrame to extract a year from.
+        year (int): The year to extract.
+
+    Returns:
+        pd.DataFrame: The extracted year.
+    """
+    return df[df.index.year == year]  # type: ignore
+
+
+def split_by_year(data: pd.DataFrame) -> list[pd.DataFrame]:
+    """
+    Split data by year
+    """
+    grouped = data.groupby(data.index.year)  # type: ignore
+    data_by_year = [group for _, group in grouped]
+    return data_by_year
+
+
+def complete_a_year(df: pd.DataFrame, year: int, interval: int = 15) -> pd.DataFrame:
+    """
+    Complete a year in a DataFrame by adding new data points between existing data points.
+    """
+    year_df = extract_a_year(df, year)
+
+    if len(year_df) == 0:
+        raise ValueError("Year is not in DataFrame")
+
+    completed_df = pd.concat(
+        [
+            complete_data(data, interval_seconds=interval)
+            for data in day.split_by_day(year_df)
+        ]
+    )
+
+    return completed_df
