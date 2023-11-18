0acfc377a1116d49474e8df7571d254208949bf7
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-11-18 13:20:39 +0100

    ♻️  Refactor main function to accept command line arguments

--- pfe_preprocessing/main.py
+++ pfe_preprocessing/main.py
@@ -2,11 +2,13 @@
 Main file to feed kafka with data from 2022-04-05
 """
 
+import argparse
 import logging
 from typing import Dict, cast
 
-import pandas as pd
+from confluent_kafka.schema_registry.error import SchemaRegistryError
 
+import pandas as pd
 from pfe_preprocessing.constant import DATA_PATH, StockChartType
 from pfe_preprocessing.data_agregation.helpers import save_date_data
 from pfe_preprocessing.data_agregation.reader import gather_data
@@ -18,7 +20,7 @@ from pfe_preprocessing.data_completion.helpers import (
 from pfe_preprocessing.decorators import performance_timer_decorator
 from pfe_preprocessing.kafka.admin import AdminClient
 from pfe_preprocessing.kafka.data_pipeline import DataPipeline
-from pfe_preprocessing.kafka.producer import AIOProducer
+from pfe_preprocessing.kafka.producer import AIOProducer, AvroService
 from pfe_preprocessing.utils.logger import setup_logs
 
 logger = logging.getLogger("pfe_preprocessing")
@@ -62,21 +64,23 @@ def extract_day(day: str) -> None:
     save_date_data(result, day)
 
 
-def main() -> None:
+def main(
+    day: str,
+    skip_day_extraction: bool,
+    skip_topic_creation: bool,
+    skip_schema_creation: bool,
+) -> None:
     """
     Main function to feed kafka with data from 2022-04-05
     """
     setup_logs("pfe_preprocessing")
-    DAY = "2022-04-05"
-    SKIP_DAY_EXTRACTION = True
-    SKIP_TOPIC_CREATION = False
 
-    if not SKIP_DAY_EXTRACTION:
-        extract_day(day=DAY)
+    if not skip_day_extraction:
+        extract_day(day=day)
     else:
         logger.info("Skipping day extraction")
 
-    data = load_data(f"chart_{DAY}")
+    data = load_data(f"chart_{day}")
     data = preprocess_data(data)
     grouped = group_by_ticker(data)
 
@@ -86,23 +90,35 @@ def main() -> None:
 
     configs = {"bootstrap.servers": "localhost:9092"}
 
-    data_pipeline = DataPipeline(configs, interval_seconds=1)
-    data_pipeline.prepare(grouped, consumer_num_processes=8, producer_num_processes=2)
+    data_pipeline = DataPipeline(configs, interval_seconds=3)
+    data_pipeline.prepare(grouped, consumer_num_processes=8, producer_num_processes=4)
 
     # Create topics for each ticker
-    if not SKIP_TOPIC_CREATION:
+    if not skip_topic_creation:
         admin_client = AdminClient(configs)
-        admin_client.delete_topics(list(grouped.keys()))
-        admin_client.create_topics(list(grouped.keys()))
+        topics_list = [f"market-data.{ticker}" for ticker in grouped.keys()]
+        admin_client.delete_topics(topics_list)
+        admin_client.create_topics(topics_list)
+        if not skip_schema_creation:
+            schema_str = AvroService.get_schema_from_file(
+                AvroService.SCHEMA_URL, AvroService.SCHEMA_FILE
+            )[1]
+            try:
+                for topic in topics_list:
+                    AvroService().update_schema(
+                        AvroService.SCHEMA_URL, f"{topic}-value", schema_str
+                    )
+            except SchemaRegistryError:
+                for topic in topics_list:
+                    AvroService().register_schema(
+                        AvroService.SCHEMA_URL, f"{topic}-value", schema_str
+                    )
     else:
         logger.info("Skipping topic creation")
 
     # Produce an init message for each ticker
     # Could take a while to index all the topics
     producer = AIOProducer(configs)
-
-    for ticker, _ in list(grouped.items()):
-        producer.produce(ticker, "init", "init")
 
     data_pipeline.start_producers()
     producer.flush()
@@ -112,4 +128,24 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Customize parameters")
+
+    parser.add_argument("--day", default="2022-04-05", help="Specify the day")
+    parser.add_argument(
+        "--skip-day-extraction", action="store_true", help="Skip day extraction"
+    )
+    parser.add_argument(
+        "--skip-topic-creation", action="store_true", help="Skip topic creation"
+    )
+    parser.add_argument(
+        "--skip-schema-creation", action="store_true", help="Skip schema creation"
+    )
+
+    args = parser.parse_args()
+
+    main(
+        args.day,
+        args.skip_day_extraction,
+        args.skip_topic_creation,
+        args.skip_schema_creation,
+    )
