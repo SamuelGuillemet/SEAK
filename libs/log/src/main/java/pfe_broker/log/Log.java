675f5683eaf82ba7d9fb809a6b7910efd659d2a6
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-11-26 21:29:28 +0100

    📝 Add documentation

--- libs/log/src/main/java/pfe_broker/log/Log.java
+++ libs/log/src/main/java/pfe_broker/log/Log.java
@@ -4,7 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Log {
-
+  /**
+   * Logger attached to the `pfe_broker` logger.
+   */
   public static final Logger LOG;
 
   static {
