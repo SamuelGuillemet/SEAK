08e06a2dc77907092c0f2b0d9e614f63b5b3e6b3
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-12-06 01:10:33 +0100

    ♻️ Add KafkaStreamUncaughtExceptionHandler in common

--- components/order-stream/src/main/java/pfe_broker/order_stream/OrderStream.java
+++ components/order-stream/src/main/java/pfe_broker/order_stream/OrderStream.java
@@ -18,6 +18,7 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 import pfe_broker.avro.Order;
 import pfe_broker.avro.RejectedOrder;
+import pfe_broker.common.KafkaStreamUncaughtExceptionHandler;
 
 @Factory
 public class OrderStream {
@@ -68,6 +69,11 @@ public class OrderStream {
     processAcceptedAndRejectedOrders(integrityCheckedOrderStream);
 
     return orderStream;
+  }
+
+  @Singleton
+  KafkaStreamUncaughtExceptionHandler uncaughtExceptionHandler() {
+    return new KafkaStreamUncaughtExceptionHandler();
   }
 
   private void processAcceptedAndRejectedOrders(
