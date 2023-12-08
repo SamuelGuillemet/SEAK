4749bfbca896ba8a50df6f1c5c35815751a0f9f5
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-12-08 16:21:08 +0100

    ✅ Add ApplicationMessageCrackerTest and update application.yml

---
+++ components/quickfix-server/src/test/java/pfe_broker/quickfix_server/ApplicationMessageCrackerTest.java
@@ -0,0 +1,17 @@
+package pfe_broker.quickfix_server;
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+@MicronautTest
+public class ApplicationMessageCrackerTest {
+
+  @Inject
+  private ApplicationMessageCracker applicationMessageCracker;
+
+  @Test
+  public void test() {
+    // TODO
+  }
+}
