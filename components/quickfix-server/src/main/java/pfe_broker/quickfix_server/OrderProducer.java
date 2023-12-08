01d9368dca37a470e7c64f1ee57974209a86dab4
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-12-08 02:20:56 +0100

    ✨ Add OrderProducer interface for sending orders via Kafka

---
+++ components/quickfix-server/src/main/java/pfe_broker/quickfix_server/OrderProducer.java
@@ -0,0 +1,12 @@
+package pfe_broker.quickfix_server;
+
+import io.micronaut.configuration.kafka.annotation.KafkaClient;
+import io.micronaut.configuration.kafka.annotation.KafkaKey;
+import io.micronaut.configuration.kafka.annotation.Topic;
+import pfe_broker.avro.Order;
+
+@KafkaClient
+public interface OrderProducer {
+  @Topic("${kafka.topics.orders}")
+  void sendOrder(@KafkaKey String key, Order order);
+}
