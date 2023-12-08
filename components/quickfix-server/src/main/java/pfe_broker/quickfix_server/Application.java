988c8693815cf5a08d84b584321699e9221ec3e4
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-12-08 01:26:18 +0100

    ✨ Add Application class for QuickFIX/J server

---
+++ components/quickfix-server/src/main/java/pfe_broker/quickfix_server/Application.java
@@ -0,0 +1,23 @@
+package pfe_broker.quickfix_server;
+
+import static pfe_broker.log.Log.LOG;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+  static {
+    setProperties();
+  }
+
+  public static void main(String[] args) {
+    LOG.info("Starting QuickFIX/J server");
+    Micronaut.run(Application.class, args);
+  }
+
+  public static void setProperties() {
+    System.setProperty(
+      "micronaut.config.files",
+      "classpath:application.yml,classpath:kafka.yml,classpath:quickfix.yml"
+    );
+  }
+}
