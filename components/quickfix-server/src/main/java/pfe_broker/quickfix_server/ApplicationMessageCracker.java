468f7fe45ecad74e2d6eb91c81398e53827e4c5d
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-12-11 01:48:25 +0100

    🔊 Add loggers

--- components/quickfix-server/src/main/java/pfe_broker/quickfix_server/ApplicationMessageCracker.java
+++ components/quickfix-server/src/main/java/pfe_broker/quickfix_server/ApplicationMessageCracker.java
@@ -1,9 +1,11 @@
 package pfe_broker.quickfix_server;
 
-import static pfe_broker.log.Log.LOG;
-
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pfe_broker.avro.RejectedOrder;
 import pfe_broker.avro.Trade;
 import pfe_broker.models.repositories.UserRepository;
@@ -14,13 +16,21 @@ import quickfix.IncorrectDataFormat;
 import quickfix.IncorrectTagValue;
 import quickfix.Message;
 import quickfix.MessageCracker;
+import quickfix.Session;
 import quickfix.SessionID;
+import quickfix.SessionNotFound;
 import quickfix.UnsupportedMessageType;
 
 @Singleton
 public class ApplicationMessageCracker
   extends MessageCracker
   implements Application {
+
+  private static final Logger LOG = LoggerFactory.getLogger(
+    ApplicationMessageCracker.class
+  );
+
+  Map<String, SessionID> sessionIDMap = new HashMap<>();
 
   @SuppressWarnings("unused")
   @Inject
@@ -43,7 +53,18 @@ public class ApplicationMessageCracker
   public void toAdmin(Message message, SessionID sessionId) {}
 
   @Override
-  public void fromAdmin(Message message, SessionID sessionId) {}
+  public void fromAdmin(Message message, SessionID sessionId) {
+    try {
+      String username = message
+        .getHeader()
+        .getString(quickfix.field.SenderCompID.FIELD);
+      if (message.isAdmin() && message instanceof quickfix.fix42.Logon) {
+        sessionIDMap.put(username, sessionId);
+      }
+    } catch (FieldNotFound e) {
+      e.printStackTrace();
+    }
+  }
 
   @Override
   public void toApp(Message message, SessionID sessionId) throws DoNotSend {}
@@ -53,6 +74,24 @@ public class ApplicationMessageCracker
     throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
     LOG.debug("Received message: {}", message);
     crack(message, sessionId);
+  }
+
+  @SuppressWarnings("unused")
+  private void sendMessage(Message message, String username) {
+    SessionID sessionID = sessionIDMap.get(username);
+    try {
+      Session.sendToTarget(message, sessionID);
+    } catch (SessionNotFound sessionNotFound) {
+      LOG.error("Session not found for user {}", sessionID);
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private void buildKafkaKey(Message message) throws FieldNotFound {
+    String key =
+      message.getString(quickfix.field.ClOrdID.FIELD) +
+      ":" +
+      message.getHeader().getString(quickfix.field.SenderCompID.FIELD);
   }
 
   public void sendExecutionReport(String key, Trade trade) {
