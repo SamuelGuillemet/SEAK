8ebb45ee9f7e023a587b72e116a24c911f765f78
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-12-11 01:10:50 +0100

    🚚 Rename Application class for QuickFIX/J server

--- components/quickfix-server/src/main/java/pfe_broker/quickfix_server/Application.java
+++ components/quickfix-server/src/main/java/pfe_broker/quickfix_server/Application.java
@@ -1,13 +1,15 @@
 package pfe_broker.quickfix_server;
 
-import static pfe_broker.log.Log.LOG;
-
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
   static {
     setProperties();
   }
+
+  private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
   public static void main(String[] args) {
     LOG.info("Starting QuickFIX/J server");
