d80b95b8e4db1d5afc7ac0f9edb1ed7759770e67
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-12-13 03:16:10 +0100

    🐛 Remove try catch inside test

--- components/quickfix-server/src/test/java/pfe_broker/quickfix_server/ServerApplicationTest.java
+++ components/quickfix-server/src/test/java/pfe_broker/quickfix_server/ServerApplicationTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import io.micronaut.context.annotation.Property;
 import io.micronaut.core.annotation.NonNull;
@@ -24,22 +23,20 @@ import pfe_broker.models.domains.User;
 import pfe_broker.models.repositories.UserRepository;
 import pfe_broker.quickfix_server.mocks.MockOrderListener;
 import quickfix.FieldNotFound;
-import quickfix.Message;
+import quickfix.IncorrectTagValue;
 import quickfix.SessionID;
+import quickfix.UnsupportedMessageType;
 import quickfix.field.MDEntryType;
 import quickfix.field.MDReqID;
 import quickfix.field.MDUpdateType;
 import quickfix.field.MarketDepth;
-import quickfix.field.MsgType;
-import quickfix.field.NoMDEntries;
-import quickfix.field.NoRelatedSym;
 import quickfix.field.SenderCompID;
 import quickfix.field.SubscriptionRequestType;
 import quickfix.field.Symbol;
 import quickfix.field.TargetCompID;
-import quickfix.fix42.MarketDataRequest;
-import quickfix.fix42.MarketDataSnapshotFullRefresh;
-import quickfix.fix42.NewOrderSingle;
+import quickfix.fix44.MarketDataRequest;
+import quickfix.fix44.MarketDataSnapshotFullRefresh;
+import quickfix.fix44.NewOrderSingle;
 
 @MicronautTest(
   rollback = false,
@@ -93,59 +90,69 @@ public class ServerApplicationTest implements TestPropertyProvider {
   }
 
   @Test
-  public void testOnMessageNewOrderSingle(MockOrderListener mockOrderListener) {
-    NewOrderSingle newOrderSingle = new NewOrderSingle();
+  public void testOnMessageNewOrderSingle(MockOrderListener mockOrderListener)
+    throws FieldNotFound, UnsupportedMessageType, IncorrectTagValue {
+    NewOrderSingle newOrderSingle = new NewOrderSingle(
+      new quickfix.field.ClOrdID("1"),
+      new quickfix.field.Side(quickfix.field.Side.BUY),
+      new quickfix.field.TransactTime(),
+      new quickfix.field.OrdType(quickfix.field.OrdType.MARKET)
+    );
     newOrderSingle.set(new quickfix.field.Symbol("AAPL"));
     newOrderSingle.set(new quickfix.field.OrderQty(10));
-    newOrderSingle.set(new quickfix.field.Side(quickfix.field.Side.BUY));
     newOrderSingle.getHeader().setString(SenderCompID.FIELD, "testuser");
-    try {
-      serverApplication.onMessage(newOrderSingle, new SessionID("FIX.4.2", "testuser", "SERVER"));
 
-      await()
+    serverApplication.onMessage(
+      newOrderSingle,
+      new SessionID("FIX.4.4", "testuser", "SERVER")
+    );
+
+    await()
       .pollInterval(Duration.ofSeconds(1))
       .atMost(Duration.ofSeconds(10))
       .untilAsserted(() -> {
         assertThat(mockOrderListener.receivedOrders).hasSize(1);
       });
-
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 
   @Test
-    void createMarketDataSnapshotTest() throws FieldNotFound {
-        MarketDataRequest marketDataRequest = new MarketDataRequest();
+  void createMarketDataSnapshotTest() throws FieldNotFound {
+    MarketDataRequest marketDataRequest = new MarketDataRequest();
 
-        marketDataRequest.set(new MDReqID("1"));
-        marketDataRequest.set(new SubscriptionRequestType(SubscriptionRequestType.SNAPSHOT_UPDATES));
-        marketDataRequest.set(new MarketDepth(0));
-        marketDataRequest.set(new MDUpdateType(MDUpdateType.FULL_REFRESH));
-        marketDataRequest.getHeader().setField(new SenderCompID("user1"));
-        marketDataRequest.getHeader().setField(new TargetCompID("SERVER"));
+    marketDataRequest.set(new MDReqID("1"));
+    marketDataRequest.set(
+      new SubscriptionRequestType(SubscriptionRequestType.SNAPSHOT_UPDATES)
+    );
+    marketDataRequest.set(new MarketDepth(0));
+    marketDataRequest.set(new MDUpdateType(MDUpdateType.FULL_REFRESH));
+    marketDataRequest.getHeader().setField(new SenderCompID("user1"));
+    marketDataRequest.getHeader().setField(new TargetCompID("SERVER"));
 
-        MarketDataRequest.NoRelatedSym relatedSymbolGroup1 = new MarketDataRequest.NoRelatedSym();
-        relatedSymbolGroup1.set(new Symbol("GOOGL"));
-        marketDataRequest.addGroup(relatedSymbolGroup1);
+    MarketDataRequest.NoRelatedSym relatedSymbolGroup1 =
+      new MarketDataRequest.NoRelatedSym();
+    relatedSymbolGroup1.set(new Symbol("GOOGL"));
+    marketDataRequest.addGroup(relatedSymbolGroup1);
 
-        MarketDataRequest.NoMDEntryTypes entryTypeGroup1 = new MarketDataRequest.NoMDEntryTypes();
-        entryTypeGroup1.set(new MDEntryType(MDEntryType.BID));
-        marketDataRequest.addGroup(entryTypeGroup1);
+    MarketDataRequest.NoMDEntryTypes entryTypeGroup1 =
+      new MarketDataRequest.NoMDEntryTypes();
+    entryTypeGroup1.set(new MDEntryType(MDEntryType.BID));
+    marketDataRequest.addGroup(entryTypeGroup1);
 
-        MarketDataRequest.NoRelatedSym relatedSymbolGroup2 = new MarketDataRequest.NoRelatedSym();
-        relatedSymbolGroup2.set(new Symbol("AAPL"));
-        marketDataRequest.addGroup(relatedSymbolGroup2);
+    MarketDataRequest.NoRelatedSym relatedSymbolGroup2 =
+      new MarketDataRequest.NoRelatedSym();
+    relatedSymbolGroup2.set(new Symbol("AAPL"));
+    marketDataRequest.addGroup(relatedSymbolGroup2);
 
-        MarketDataRequest.NoMDEntryTypes entryTypeGroup2 = new MarketDataRequest.NoMDEntryTypes();
-        entryTypeGroup2.set(new MDEntryType(MDEntryType.BID));
-        marketDataRequest.addGroup(entryTypeGroup2);
+    MarketDataRequest.NoMDEntryTypes entryTypeGroup2 =
+      new MarketDataRequest.NoMDEntryTypes();
+    entryTypeGroup2.set(new MDEntryType(MDEntryType.BID));
+    marketDataRequest.addGroup(entryTypeGroup2);
 
-        MarketDataSnapshotFullRefresh snapshot = null;
-        snapshot = serverApplication.createMarketDataSnapshot(marketDataRequest);
-        
-        assertNotNull(snapshot);
-        assertEquals("1", snapshot.getString(MDReqID.FIELD));
-        assertEquals(2, snapshot.getInt(NoMDEntries.FIELD));
-    }
+    MarketDataSnapshotFullRefresh snapshot = null;
+    snapshot = serverApplication.createMarketDataSnapshot(marketDataRequest);
+
+    assertNotNull(snapshot);
+    assertEquals("1", snapshot.getMDReqID().getValue());
+    assertEquals(2, snapshot.getNoMDEntries().getValue());
+  }
 }
