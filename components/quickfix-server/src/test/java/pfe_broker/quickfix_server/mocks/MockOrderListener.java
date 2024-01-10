df051bedc24fecbeacfb42245c37304574e3248d
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2024-01-10 16:02:52 +0100

    🤡 Add Kafka listeners and producers for OrderBookRequest

--- components/quickfix-server/src/test/java/pfe_broker/quickfix_server/mocks/MockOrderListener.java
+++ components/quickfix-server/src/test/java/pfe_broker/quickfix_server/mocks/MockOrderListener.java
@@ -7,15 +7,23 @@ import jakarta.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
 import pfe_broker.avro.Order;
+import pfe_broker.avro.OrderBookRequest;
 
 @Singleton
 public class MockOrderListener {
 
   public List<Order> receivedOrders = new ArrayList<>();
+  public List<OrderBookRequest> receivedOrderBookRequests = new ArrayList<>();
 
   @KafkaListener("mock-orders-consumer")
   @Topic("${kafka.topics.orders}")
   void receiveOrder(@KafkaKey String key, Order order) {
     receivedOrders.add(order);
+  }
+
+  @KafkaListener("mock-order-book-request-consumer")
+  @Topic("${kafka.topics.order-book-request}")
+  void receiveOrderBookRequest(@KafkaKey String key, OrderBookRequest order) {
+    receivedOrderBookRequests.add(order);
   }
 }
