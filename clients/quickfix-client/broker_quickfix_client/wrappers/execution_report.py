02b954e4171058b94ff4771b23bc231321302a7f
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2024-01-13 21:13:32 +0100

    ♻️ Refactor execution report data classes

--- clients/quickfix-client/broker_quickfix_client/wrappers/execution_report.py
+++ clients/quickfix-client/broker_quickfix_client/wrappers/execution_report.py
@@ -8,23 +8,38 @@ from broker_quickfix_client.wrappers.enums import (
 
 
 @dataclass
-class FilledExecutionReport:
+class BaseExecutionReport:
     order_id: int
     client_order_id: int
     symbol: str
     side: SideEnum
-    type: OrderTypeEnum
     leaves_quantity: int
-    price: float
-    cum_quantity: int
+    type: OrderTypeEnum
 
 
 @dataclass
-class RejectedExecutionReport:
-    order_id: int
-    client_order_id: int
-    symbol: str
-    side: SideEnum
-    type: OrderTypeEnum
-    leaves_quantity: int
+class FilledExecutionReport(BaseExecutionReport):
+    cum_quantity: int
+    price: float
+
+
+@dataclass
+class RejectedExecutionReport(BaseExecutionReport):
     reject_reason: OrderRejectReasonEnum
+
+
+@dataclass
+class AcceptedOrderExecutionReport(BaseExecutionReport):
+    price: float
+
+
+@dataclass
+class ReplacedOrderExecutionReport(BaseExecutionReport):
+    original_client_order_id: int
+    price: float
+
+
+@dataclass
+class CanceledOrderExecutionReport(BaseExecutionReport):
+    original_client_order_id: int
+    price: float
