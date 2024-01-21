7dbd2f5868d922d1803979f4c065770c5176c42e
TJA <theojosephantoine@gmail.com>
2024-01-21 14:43:17 +0100

    ✨ first working version of the interface

---
+++ clients/quickfix-client/interface/account_window.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import ttk
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+
+class AccountWindow(Toplevel):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Owned Shares Table")
+        # Define the columns
+        owned_shares_columns = ("Share_name", "quantity")
+
+        # Create a Treeview widget
+        owned_shares_tree = ttk.Treeview(self, columns=owned_shares_columns, show="headings")
+
+        # Configure column headings
+        for col in owned_shares_columns:
+            owned_shares_tree.heading(col, text=col)
+            owned_shares_tree.column(col, width=100, anchor=tk.CENTER)
+
+        # Insert data into the Treeview
+        if self.master.owned_shares:
+            for share_name, quantity in self.master.owned_shares.items():
+                owned_shares_tree.insert("", "end", values=(share_name, quantity))
+
+        # Pack the Treeview
+        owned_shares_tree.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
---
+++ clients/quickfix-client/run_interface.py
@@ -0,0 +1,5 @@
+from interface.login_window import LoginWindow
+
+if __name__ == "__main__":
+    app = LoginWindow()
+    app.mainloop()
