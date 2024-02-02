c6f306e408fccd146445bfff08280bb4ee0ebd5d
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2024-02-02 12:26:51 +0100

    ✨ Add new database files and functions utilities

---
+++ components/website-backend/app/db/databases/database_interface.py
@@ -0,0 +1,72 @@
+from abc import ABC, abstractmethod
+from typing import Any, AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import Session
+
+from app.db.base_class import Base
+
+
+class DatabaseInterface(ABC):
+    def __init__(self):
+        self.async_sessionmaker: async_sessionmaker[AsyncSession] | None = None
+        self.async_engine: AsyncEngine | None = None
+
+    async def __call__(self) -> AsyncGenerator[AsyncSession, Any]:
+        """
+        Create a new session to interact with the database.
+        To be used by FastAPI's dependency injection system.
+        see https://fastapi.tiangolo.com/tutorial/dependencies/dependencies-with-yield/#a-database-dependency-with-yield
+        ! This Must only be used by FastAPI's dependency injection system !
+        """
+
+        session = self.get_session()
+
+        try:
+            yield session
+        finally:
+            await session.close()
+
+    @abstractmethod
+    def setup(self) -> async_sessionmaker[AsyncSession]:  # pragma: no cover
+        ...
+
+    @abstractmethod
+    async def drop(self) -> None:  # pragma: no cover
+        ...
+
+    async def shutdown(self) -> None:
+        """
+        Close the SQLAlchemy engine.
+        """
+        if self.async_engine:
+            await self.async_engine.dispose()
+
+    def get_session(self) -> AsyncSession:
+        """
+        Create a new session to interact with the database in a synchronous way.
+        You need to close the session after using it.
+        """
+        if not self.async_sessionmaker:
+            raise RuntimeError("Database not initialized")
+
+        return self.async_sessionmaker()
+
+    async def create_all(self, no_drop: bool = False) -> None:
+        """
+        Create all tables, with a drop first if they already exist.
+        """
+
+        def wrapper(session: Session, method: Any) -> Any:
+            """
+            Wrapper to get a connection from the session and close it after the method is called.
+            """
+            connection = session.connection()
+            return method(connection)
+
+        metadata = Base.metadata
+        async with self.get_session() as session:
+            async with session.begin():
+                if not no_drop:
+                    await session.run_sync(wrapper, metadata.drop_all)
+                await session.run_sync(wrapper, metadata.create_all)
---
+++ components/website-backend/app/db/databases/sqlite.py
@@ -0,0 +1,41 @@
+import os
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.core.config import settings
+from app.db.databases.database_interface import DatabaseInterface
+
+
+class SqliteDatabase(DatabaseInterface):
+    ERROR_MESSAGE = "Don't use SQLite in production"
+
+    def setup(
+        self, path: str = settings.DATABASE_URI
+    ) -> async_sessionmaker[AsyncSession]:
+        """
+        Create a new SQLAlchemy engine and sessionmaker.
+        """
+        if settings.ENVIRONMENT == "production":
+            raise ValueError(self.ERROR_MESSAGE)
+
+        self.async_engine: AsyncEngine = create_async_engine(path)
+        self.async_sessionmaker = async_sessionmaker(
+            self.async_engine, class_=AsyncSession, expire_on_commit=False
+        )
+        return self.async_sessionmaker
+
+    async def drop(self, path: str = settings.DATABASE_URI) -> None:
+        """
+        Drop the database, by deleting the db file.
+        """
+        if settings.ENVIRONMENT == "production":
+            raise ValueError(self.ERROR_MESSAGE)
+
+        file_name = path.split("sqlite:///")[-1]
+        if os.path.exists(file_name):  # pragma: no cover
+            os.remove(file_name)
