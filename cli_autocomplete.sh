e2cd0219a09b5f9056a1b1d14d48c833c76182ad
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2024-02-06 01:57:34 +0100

    🔨 Add website-backend and website-frontend projects to cli

--- cli_autocomplete.sh
+++ cli_autocomplete.sh
@@ -9,9 +9,11 @@ _cli_autocomplete() {
   commands="docker docker_multi reload_grafana reload_prometheus project help"
   docker_actions="start restart stop down"
   java_projects="market-matcher order-book order-stream trade-stream quickfix-server"
-  python_projects="pre-processing"
+  python_projects="pre-processing website-backend"
+  npm_projects="website-frontend"
   java_actions="build run test coverage"
   python_actions="run test"
+  npm_actions="run test"
 
   case "$prev" in
   docker | docker_multi)
@@ -19,7 +21,7 @@ _cli_autocomplete() {
     return 0
     ;;
   project)
-    mapfile -t COMPREPLY < <(compgen -W "$java_projects $python_projects" -- "$cur")
+    mapfile -t COMPREPLY < <(compgen -W "$java_projects $python_projects $npm_projects" -- "$cur")
     return 0
     ;;
   # Any of the java projects
@@ -28,8 +30,13 @@ _cli_autocomplete() {
     return 0
     ;;
   # Any of the python projects
-  pre-processing)
+  pre-processing | website-backend)
     mapfile -t COMPREPLY < <(compgen -W "$python_actions" -- "$cur")
+    return 0
+    ;;
+  # Any of the npm projects
+  website-frontend)
+    mapfile -t COMPREPLY < <(compgen -W "$npm_actions" -- "$cur")
     return 0
     ;;
   *)
