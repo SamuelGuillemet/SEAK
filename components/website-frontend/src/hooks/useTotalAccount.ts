6e90ac7b4be35974ed11d5a07f076a83eccd4a26
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2024-02-06 01:56:53 +0100

    ✨ Add useOnEchap and useTotalAccount hooks

---
+++ components/website-frontend/src/hooks/useTotalAccount.ts
@@ -0,0 +1,24 @@
+import { useMemo } from "react";
+
+import { RankedAccount } from "@/openapi-codegen/apiSchemas";
+
+interface UseTotalAccountProps {
+  account: RankedAccount;
+  marketDataPrices: Record<string, number>;
+}
+
+export function calculateTotalAccount(account: RankedAccount, marketDataPrices: Record<string, number>) {
+  return (
+    (account.balance ?? 0) +
+    (account.stocks?.reduce((acc, stock) => {
+      const price = marketDataPrices[stock.symbol] ?? 0;
+      return acc + stock.quantity * price;
+    }, 0) ?? 0)
+  );
+}
+
+export function useTotalAccount(props: Readonly<UseTotalAccountProps>) {
+  return useMemo(() => {
+    return calculateTotalAccount(props.account, props.marketDataPrices);
+  }, [props.account, props.marketDataPrices]);
+}
