191ab2c1a5c671b04b2f0c790eb62762c5fe96df
TheoJ-A <theojosephantoine@gmail.com>
2024-02-06 11:03:30 -0500

    ✨ function to create candlestick charts

--- clients/graphical-user-interface/quickfix_client_gui/interface/candlestick_chart.py
+++ clients/graphical-user-interface/quickfix_client_gui/interface/candlestick_chart.py
@@ -1,26 +1,39 @@
-# import mplfinance as mpf
-# from broker_quickfix_client.wrappers.market_data import MarketDataResponse
+import os
 
-# def create_candlestick_chart(market_data_response: MarketDataResponse):
-#     symbol = market_data_response.symbol
+import mplfinance as mpf
+import pandas as pd
+from broker_quickfix_client.wrappers.market_data import MarketDataResponse
+from mplfinance.dates import date2num
 
-#     # Extract relevant data for the first symbol
-#     entries = market_data_response.market_data.get(symbol, [])
 
-#     # Prepare candlestick data
-#     ohlc_data = []
-#     for entry in entries:
-#         ohlc_data.append((
-#             date2num(entry.md_entry_time),  # Convert time to matplotlib format
-#             entry.md_entry_px.open,
-#             entry.md_entry_px.high,
-#             entry.md_entry_px.low,
-#             entry.md_entry_px.close,
-#         ))
+def create_candlestick_chart(market_data_response: MarketDataResponse):
+    symbol = market_data_response.symbol
 
-#     # Create a simple candlestick chart
-#     df = mpf.DataFrame(ohlc_data, columns=['Date', 'Open', 'High', 'Low', 'Close'])
-#     df['Date'] = pd.to_datetime(df['Date'], unit='s')
+    # Extract relevant data for the first symbol
+    entries = market_data_response.market_data.get(symbol, [])
 
-#     mpf.plot(df, type='candle', style='yahoo', title=f'Candlestick Chart - {symbol}',
-#              ylabel='Price', savefig=os.path.join('charts', f'{symbol}.png'))
+    # Prepare candlestick data
+    ohlc_data = []
+    for entry in entries:
+        ohlc_data.append(
+            (
+                date2num(entry.md_entry_time),
+                entry.md_entry_px.open,
+                entry.md_entry_px.high,
+                entry.md_entry_px.low,
+                entry.md_entry_px.close,
+            )
+        )
+
+    # Create a simple candlestick chart
+    df = mpf.DataFrame(ohlc_data, columns=["Date", "Open", "High", "Low", "Close"])
+    df["Date"] = pd.to_datetime(df["Date"], unit="s")
+
+    mpf.plot(
+        df,
+        type="candle",
+        style="yahoo",
+        title=f"Candlestick Chart - {symbol}",
+        ylabel="Price",
+        savefig=os.path.join("charts", f"{symbol}.png"),
+    )
